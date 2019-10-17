986e04886581a95c5d1f2bf960bb2e642f29d169
Karthik N P <52942938+gh05t-4@users.noreply.github.com>
2019-10-17 21:43:23 +0530

    Update text2hex.py

--- text2hex.py
+++ text2hex.py
@@ -1,7 +1,11 @@
+# Created by SKS bros.
+
 text = input("Enter the text: ")
 hexval = ""
+
 for i in text:
     h = hex(ord(i))
     hexval = hexval + h + " "
+
 print("\nHex:\n\n" + hexval)
 print("\n")
